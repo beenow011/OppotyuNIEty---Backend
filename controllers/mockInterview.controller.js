d05c8ee902119caa2ed204c4fb3662e08df620e8
beenow011 <abhinavnb11@gmail.com>
2024-11-30 09:35:26 +0530

    Add interview session management functionality; implement routes and controllers for starting, continuing, and retrieving mock interview sessions and conversations

--- controllers/mockInterview.controller.js
+++ controllers/mockInterview.controller.js
@@ -2,7 +2,9 @@ const interviewQuestionsModels = require("../models/interviewQuestions.models");
 const MockInterviewConversationModel = require("../models/mockinterviewConversations.models");
 const { default: OpenAI } = require("openai");
 const { OPENAI_API_KEY } = require("../config/config");
-const { OpenAIStream } = require("ai");
+// import { OpenAIStream, StreamingTextResponse } from 'ai';
+const { OpenAIStream, StreamingTextResponse } = require('ai');
+const InterviewSessionModel = require("../models/interviewSession.models");
 
 const openai = new OpenAI({
     apiKey: OPENAI_API_KEY
@@ -72,69 +74,153 @@ async function createInterviewSession(req, res) {
         if (!interviewSession) {
             throw new Error('Error in creating interview session');
         }
-        res.status(200).json({ message: 'Interview session created successfully' });
+        res.status(200).json({ message: 'Interview session created successfully', data: interviewSession });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: error.message });
     }
 }
 
-async function interviewSession(req, res) {
+async function getInterviewSession(req, res) {
     try {
-        const { companyId } = req.query;
+        const { id, type } = req.params;
         const studentId = req.user._id;
-        const interviewSessions = await InterviewSessionModel.find({ companyId, studentId }).populate('companyId').populate('studentId');
-        const interviewQuestions = await interviewQuestionsModels.find({ company: companyId, type: interviewSessions.interviewType });
-        const sessionQuestions = await MockInterviewConversationModel.find({ companyId, studentId }).sort({ createdAt: -1 }).limit(10);
+        const interviewSessions = await InterviewSessionModel.find({ companyId: id, studentId, interviewType: type }).populate('companyId').populate('studentId');
+        res.status(200).json({ message: 'Interview sessions fetched successfully', data: interviewSessions });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+}
 
-        const formattedPrevMessages = sessionQuestions.map((msg) => ({
-            role: msg.isUserMsg ? 'user' : 'assistant',
-            content: msg.message,
-        }));
+async function StartinterviewSession(req, res) {
+    try {
+        const { sessionId } = req.body;
+        const interviewSessions = await InterviewSessionModel.findById(sessionId).populate('companyId').populate('studentId');
 
-
-        const context = `PREVIOUS CONVERSATION:${formattedPrevMessages.map((msg) => {
-            if (msg.role === 'user') return `User:${msg.content}\n`;
-            return `Assistant:${msg.content}\n`;
-        })}`;
-
+        // console.log("4")
         const response = await openai.chat.completions.create({
             model: 'gpt-4o-mini',
             temperature: 0.7, // Adjust the temperature as needed
-            stream: true,
             messages: [
                 {
                     role: 'system',
-                    content: `Based on this job description: ${interviewSessions.companyId.jobDescription} and this skills: ${interviewSessions.skills}, generate a mock interview question tailored to the role. Focus on key skills, experiences, and common industry challenges. you also have the context to the prevoius conversation`,
+                    content: `Based on this job description: ${interviewSessions.companyId.jobDescription} and this skills: ${interviewSessions.skills}, Start the mock interview question tailored to the role. Focus on key skills, experiences, and common industry challenges. (1 QUESTION AT A TIME) response format :question`,
 
-                },
-                {
-                    role: 'user',
-                    content: context, // Provide the context here
-                },
+                }
             ],
         });
-        if (response.choices[0].message.type === 'completion') {
+
+        if (response.choices[0].message.content) {
             await MockInterviewConversationModel.create({
-                companyId,
-                studentId,
-                message: completion.choices[0].message.content,
+                sessionId,
+                message: response.choices[0].message.content,
                 isUserMsg: false,
             });
+            const completionResult = response.choices[0].message.content
+            return res.status(200).json({ message: "Interview started", data: completionResult })
+        } else {
+            throw new Error("No response")
         }
-        const completionResult = response.choices[0].message.content;
 
-        return res.status(200).json({ message: "Resume Sent", data: completionResult })
+
+
 
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: err.message });
     }
 }
+async function continueInterviewSession(req, res) {
+    try {
+        const { sessionId, message } = req.body;
+        if (!message) {
+            throw new Error("Message is required");
+        }
+
+        const msg = await MockInterviewConversationModel.create({
+            sessionId,
+            message,
+            isUserMsg: true,
+        });
+        if (!msg) {
+            throw new Error("Error in saving message");
+        }
+        const session = await InterviewSessionModel.findById(sessionId);
+        if (!session) {
+            throw new Error("Session not found");
+        }
+        const sessionQuestions = await MockInterviewConversationModel.find({ sessionId }).sort({ createdAt: -1 }).limit(10);
+        const formattedPrevMessages = sessionQuestions.map((msg) => ({
+            role: msg.isUserMsg ? 'user' : 'assistant',
+            content: msg.message,
+        }));
+        const context = `PREVIOUS CONVERSATION:${formattedPrevMessages.map((msg) => {
+            if (msg.role === 'user') return `User:${msg.content}\n`;
+            return `Assistant:${msg.content}\n`;
+        })}`;
+        const response = await openai.chat.completions.create({
+            model: 'gpt-4o-mini',
+            temperature: 0.7, // Adjust the temperature as needed
+            messages: [
+                {
+                    role: 'system',
+                    content: `Based on this job description: ${session.companyId.jobDescription} and this skills: ${session.skills}, Continue the mock interview question as per context tailored to the role. Focus on key skills, experiences, and common industry challenges. you also have the context to the prevoius conversation (1 question at a time). response format :question`,
+
+                },
+                {
+                    role: 'user',
+                    content: context, // Provide the context here
+                },
+                {
+                    role: 'user',
+                    content: message
+                }
+            ],
+        });
+        if (response.choices[0].message.content) {
+            await MockInterviewConversationModel.create({
+                sessionId,
+                message: response.choices[0].message.content,
+                isUserMsg: false,
+            });
+            const completionResult = response.choices[0].message.content
+            return res.status(200).json({ message: "Interview started", data: completionResult })
+        } else {
+            throw new Error("No response")
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+}
+async function MockInterviewConversation(req, res) {
+    try {
+        const { sessionId } = req.body;
+        console.log("Session ID:", sessionId);
+
+        //sessionId is the _id of the interview session
+        // const sessionQuestions = await MockInterviewConversationModel.find({ sessionId }).sort({ createdAt: -1 }).limit(10);
+        //why am i getting empty array
+        // const sessionQuestions = await MockInterviewConversationModel.find({ sessionId }).sort({ createdAt: -1 });
+        const sessionQuestions = await MockInterviewConversationModel.find({ sessionId }).sort({ createdAt: 1 });
+        console.log(sessionQuestions)
+
+        return res.status(200).json({ message: "Interview Questions", data: sessionQuestions })
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+}
+
 module.exports = {
     uploadInterviewQuestions,
     getInterviewQuestions,
     getInterviewQuestionsOfType,
     createInterviewSession,
-    interviewSession
+    StartinterviewSession,
+    continueInterviewSession,
+    getInterviewSession,
+    MockInterviewConversation
 }
--- routes/mockInterview.routes.js
+++ routes/mockInterview.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { verifyCoordinator, verifyStudentOrCoordinator, verifyStudent } = require('../middleware/auth');
-const { uploadInterviewQuestions, getInterviewQuestions, getInterviewQuestionsOfType, createInterviewSession } = require('../controllers/mockInterview.controller');
+const { uploadInterviewQuestions, getInterviewQuestions, getInterviewQuestionsOfType, createInterviewSession, getInterviewSession, MockInterviewConversation, StartinterviewSession, continueInterviewSession } = require('../controllers/mockInterview.controller');
 
 
 const router = express.Router();
@@ -9,6 +9,10 @@ router.post('/mock-interview/upload-interview-questions/:id', verifyCoordinator,
 router.get('/mock-interview/get-interview-questions/:id', verifyStudentOrCoordinator, getInterviewQuestions);
 router.get('/mock-interview/get-interview-questions/:id/:type', verifyStudent, getInterviewQuestionsOfType);
 router.post('/mock-interview/create-interview-session', verifyStudent, createInterviewSession);
+router.get('/mock-interview/get-interview-session/:id/:type', verifyStudent, getInterviewSession);
+router.post('/mock-interview/start-interview-session', verifyStudent, StartinterviewSession);
+router.post('/mock-interview/continue-interview-conversation', verifyStudent, continueInterviewSession);
+router.post('/mock-interview/get-interview-conversation', verifyStudent, MockInterviewConversation);
 // router.post('/mock-interview/create-interview-session', verifyStudent, createInterviewSession);
 
 module.exports = router;
